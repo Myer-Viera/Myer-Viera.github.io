73b67e2c88907d7c90c32a704e5d37e9d6024d86
Myer Viera <Myer.Viera@gmail.com>
2026-03-09 08:02:13 -0400

    helooooo

---
+++ projects/game/README.md
@@ -0,0 +1,31 @@
+# 1st Day at Camp
+
+## Setting
+
+This game takes place at camp Airy.
+
+## Map
+
+
+The player starts at the bunks, and then is directed down village row.
+They can explore, but must eventually make their way to the gym.
+
+## Story
+at the begining you start when the bell rings to go to first period, you then have to make your way from village row down to the gym.
+
+## Global Variables
+
+The most important variables are
+`____` and `____`, both
+booleans that track progress in the
+story. Depending on these two variables,
+some rooms will display different things.
+
+I also have numeric variables called `day` and `minute` which keep track of 
+time. `minute` starts at 0 and counts up
+with each move.
+
+I have a little HUD map, and use a bunch of 
+boolean variables to control which
+rooms the player has discovered. A map is only displayed after the user
+visits it.
--- projects/game/game.js
+++ projects/game/game.js
@@ -29,11 +29,14 @@ function locationB() {
     clear();
     print("\nYou are in location B!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+        "\n\tlocationC" +
+        "\n\tlocationD");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationa") {
-            locationA();
+          if (input.toLowerCase() === "locationc") {
+            locationC();
+        } else if (input.toLowerCase() === "locationd") {
+            locationD();
         } else {
             stayHere();
             waitThenCall(locationB);
@@ -42,6 +45,39 @@ function locationB() {
     waitForInput(processInput);
 }
 
+function locationC() {
+    clear();
+    print("\nYou are in location C!");
+    print("\nWhere do you want to go next? Say one of these choices:" +
+        "\n\tlocationD");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "locationd") {
+            locationD();
+        } else {
+            stayHere();
+            waitThenCall(locationC);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function locationD() {
+    clear();
+    print("\nYou are in location D!");
+    print("\nWhere do you want to go next? Say one of these choices:" +
+        "\n\tlocationE");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "locatione") {
+            locationE();
+        } else {
+            stayHere();
+            waitThenCall(locationD);
+        }
+    }
+    waitForInput(processInput);
+}
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
